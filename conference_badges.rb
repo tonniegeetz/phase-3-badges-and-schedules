489fb201e5e7d05ae9a4043d8c37548e38216ff5
tonniegeetz <tonniegeetz@gmail.com>
2024-06-16 09:08:49 +0300

    enumerables badges

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  array = []
+  names.each do |name|
+    array << "Hello, my name is #{name}."
+  end
+  array
+end
+
+def assign_rooms(names)
+  assignments = []
+  names.each_with_index do |name, index|
+    assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  assignments
+end
+
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+  assign_rooms(names).each do |assignment|
+    puts assignment
+  end
+end
